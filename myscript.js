ed17fe3a20fcbe0677ab027752643ec8007665fe
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-03-04 09:30:52 +0200

    intro to variables

---
+++ myscript.js
@@ -0,0 +1,5 @@
+
+name = 'ahmed'
+if (name==='ahmed'){
+    console.log("hii")
+}
---
+++ variabless.py
@@ -0,0 +1,46 @@
+
+
+name = 'ahmed'
+Name = 'mohamed'
+
+if name== 'ahmed':
+    print("Hello ahmed!")
+
+
+# bio= ("My name is Noha"
+#       "I works at ITI "
+#       "I lives in cairo")
+#
+# print(bio)
+
+
+# this is a comment in python
+
+
+bio= ("My name is Noha\n"
+      "I works at ITI\n"
+      "I lives in cairo")
+
+print(bio)
+
+# python provides multi-line string
+
+bio2 = '''My name is Noha
+I works at ITI
+I lives in cairo'''
+print(bio2)
+
+paragraph ="""
+sdkjkf
+ajfh
+kaf
+"""
+
+
+"""this is considered to be a comment """
+
+'this is another comment '
+
+
+
+
