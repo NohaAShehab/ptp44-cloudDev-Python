e985382eb80e4bdb3f6482e6982513b4f79a45e5
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-03-06 09:40:33 +0200

    import modules

---
+++ module_packages.py
@@ -0,0 +1,21 @@
+
+# print("------------Nice to meet you ")
+
+
+coursename = 'admin01'
+
+def saywelcome(name="ahmed"):
+    print(f"Welcome {name}")
+
+#
+
+
+x = 10
+
+if __name__=='__main__':
+    print("---- Good morning Alaaaaaaaaaaaaa")
+    saywelcome("Alaa <3")
+    stdname='Doaa'
+    saywelcome(stdname)
+    x= 100
+
---
+++ playground.py
@@ -0,0 +1,45 @@
+
+
+""" import another module """
+""" 1- import all the module """
+
+# import module_packages
+#
+# print(module_packages.coursename)
+#
+# module_packages.saywelcome("Ali")
+
+"""alias module name"""
+#
+# import  module_packages as example_mod
+# example_mod.saywelcome("sjkh")
+
+""" import part of the module """
+# from module_packages import saywelcome
+# saywelcome("uirejh")
+# from module_packages import  coursename
+# print(coursename)
+
+""" alias imported parts """
+# from module_packages import saywelcome as welcome
+# welcome("uirejh")
+# from module_packages import  coursename as cname
+# print(cname)
+# print(coursename)
+
+
+""" check this """
+
+import module_packages
+
+# from module_packages import coursename
+
+x  = 10
+
+
+
+
+
+
+
+
